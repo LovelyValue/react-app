ebeb69c398cf7b4678573a40df0f777936a9dc49
Lovely.Value <aid.l.v.esd@gmail.com>
2024-12-17 18:09:47 +0300

    Упражнение - работа контекста

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -50,6 +50,13 @@ function JournalForm({ onSubmit }) {
 		}
 	}, [isFormReadyToSubmit, values, onSubmit]);
 
+	useEffect(() => {
+		dispatchForm({
+			type: 'SET_VALUE',
+			payload: { userId },
+		});
+	}, [userId]);
+
 	const onChange = e => {
 		dispatchForm({
 			type: 'SET_VALUE',
@@ -65,7 +72,6 @@ function JournalForm({ onSubmit }) {
 
 	return (
 		<form className={styles['journal-form']} onSubmit={addJournalItem}>
-			{userId}
 			<div>
 				<Input
 					type='text'
