5ce823baf29dbf5d703df79c122ccc9197c91d8f
Lovely.Value <aid.l.v.esd@gmail.com>
2024-12-20 18:51:17 +0300

    useCallback

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import Button from '../Button/Button';
 import Logo from '../Logo/Logo';
 import SelectUser from '../SelectUser/SelectUser';
@@ -8,9 +8,9 @@ const logos = ['/logo.svg', '/vite.svg'];
 function Header() {
 	const [logoIndex, setLogoIndex] = useState(0);
 
-	const toggleLogo = () => {
+	const toggleLogo = useCallback(() => {
 		setLogoIndex(state => Number(!state));
-	};
+	}, []);
 
 	return (
 		<>
