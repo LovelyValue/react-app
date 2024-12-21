95b0db5252ce78a4f928f335e0ce1ca8f78f2692
Lovely.Value <aid.l.v.esd@gmail.com>
2024-12-21 17:40:52 +0300

    Упражнение - Удаление записи

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,13 @@
-import { useState } from 'react';
-import Button from '../Button/Button';
 import Logo from '../Logo/Logo';
 import SelectUser from '../SelectUser/SelectUser';
 
 const logos = ['/logo.svg', '/vite.svg'];
 
 function Header() {
-	const [logoIndex, setLogoIndex] = useState(0);
-
-	const toggleLogo = () => {
-		setLogoIndex(state => Number(!state));
-	};
-
 	return (
 		<>
-			<Logo image={logos[logoIndex]} />
+			<Logo image={logos[0]} />
 			<SelectUser />
-			<Button onClick={toggleLogo}>Сменить лого</Button>
 		</>
 	);
 }
